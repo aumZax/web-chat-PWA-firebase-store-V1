4175f229dc2e0056945578ba4f9e2c0c0487f830
aumZax <oodgame.1h@gmail.com>
2025-12-18 15:48:20 +0700

    https

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -17,7 +17,9 @@ export default function LoginPage() {
     setLoading(true);
 
     try {
-      const response = await fetch("http://localhost:3000/login", {
+      // const response = await fetch("http://localhost:3000/login", {
+      const response = await fetch("https://web-chat-pwa-firebase-store-v1-api.vercel.app/login", {
+
         method: "POST",
         headers: {
           "Content-Type": "application/json",
